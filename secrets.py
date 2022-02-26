9cc9fee50ceddbb8f2aedf56b0e62a83f295e490
Evan <evanlimhj@gmail.com>
2022-02-26 13:57:33 +0800

    z

--- secrets.py
+++
@@ -1,9 +0,0 @@
-username = "cepfinal2"
-password = "securepwd"
-dbname = "cluster0"
-host = "cluster0.9x8ee.mongodb.net"
-
-zoom_apikey = "7tSdhVlLQ9-NJZEY0OCyJA"
-zoom_apisecret = "c9RsqIiZ00GXv96RorysE2zeb5SKxk6GDev8"
-
-github_pwd = "securepwd10"
