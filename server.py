234a95bd7282ccec2f7792df51620762518029b5
Evan <evanlimhj@gmail.com>
2022-02-26 14:27:56 +0800

    u may want to use this user thing

--- server.py
+++ server.py
@@ -8,11 +8,32 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
-
     
 @app.route('/meetings', methods=['GET'])
 def meetings():
     return render_template('meetings.html')
+
+# @app.route('/profile/tutor/<string:usr>')  # Username
+# def showTutorProfile(usr):  # DONE
+#     usr_doc = dbscript.getTutorDoc({
+#         'usrname': usr
+#     })
+
+#     return render_template('profile.html', usr_doc=usr_doc, type='tutor')
+
+
+# @app.route('/profile/tutee/<string:usr>')  # Username
+# def showTuteeProfile(usr):  # DONE
+#     # NOTE: If usr not found, return home and flash
+#     usr_doc = dbscript.getTuteeDoc({
+#         'usrname': usr
+#     })
+
+#     if usr_doc:
+#         return render_template('profile.html', usr_doc=usr_doc, type='tutee')
+#     else:
+#         flash("User not found", "error")
+#         return redirect(url_for('home'))
 
 if (__name__ == '__main__'):
     app.run(debug=True)
